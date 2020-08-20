0f7f7dcd3314922692a22a4dd4df609154c18b92
Anthony Tarr <me@anthonytarr.net>
2020-08-19 21:51:44 -0700

    Small changes to get a prod build working

--- electron.js
+++ electron.js
@@ -20,7 +20,7 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../dist/index.html')}`);
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => (mainWindow = null));
 }
 
